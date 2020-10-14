9f4a1742d30ab5b3c34d7693a76f37a26035e4aa
hussain89200 <h.bandukwala22@gmail.com>
2020-10-14 15:30:52 +0530

    fourth commit

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from . import models
 def index(request):
+
     return render(request, 'shop/index.html')
 
 def about(request):
