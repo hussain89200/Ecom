840faa06775c858bba93811d8ab1a87bda2e9519
hussain89200 <h.bandukwala22@gmail.com>
2020-10-27 14:19:49 +0530

    eleventh commit

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product, Contact
+from .models import Product, Contact, Orders
 from math import ceil
 def index(request):
     products = Product.objects.all()
@@ -47,5 +47,20 @@ def productView(request,myid):
     return render(request, 'shop/prodview.html', {'product':product[0]})
 
 def checkout(request):
+    if request.method == "POST":
+        items_json = request.POST.get('itemsJson','')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address1', '') + "" + request.POST.get('address2', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        phone = request.POST.get('phone', '')
+        zip_code = request.POST.get('zip_code', '')
+        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city, state=state, phone=phone, zip_code=zip_code)
+        order.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'shop/checkout.html', {'thank': thank, 'id':id})
+
     return render(request, 'shop/checkout.html')
 
