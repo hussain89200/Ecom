76e481eefce664876ca3a484d237c21229685d51
hussain89200 <h.bandukwala22@gmail.com>
2020-10-31 14:27:10 +0530

    14th commit

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -23,6 +23,7 @@ def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
+    thank = False
     if request.method == "POST":
         name = request.POST.get('name', '')
         email = request.POST.get('email', '')
@@ -31,7 +32,8 @@ def contact(request):
         desc = request.POST.get('desc', '')
         contact = Contact(name= name, email=email, city=city, phone=phone, desc=desc)
         contact.save()
-    return render(request, 'shop/contact.html')
+        thank = True
+    return render(request, 'shop/contact.html', {'thank':thank})
 
 def tracker(request):
     if request.method=="POST":
